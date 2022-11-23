7f8fe0225c35a8e08f62cf1af6c4b8465732d1e2
hongmono <hojw1019@gmail.com>
2022-11-23 11:01:57 +0900

    BWT901ClSettingDialog parameter 변경

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,11 +6,13 @@ import 'package:multiparingbase/app/data/models/signals.dart';
 import 'package:multiparingbase/app/widgets/app_drawer.dart';
 import 'package:multiparingbase/app/widgets/bwt901cl_setting_dialog.dart';
 import 'package:multiparingbase/app/widgets/bwt901cl_tile.dart';
+import 'package:multiparingbase/app/widgets/custom_floating_action_button.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,64 +20,49 @@ class HomeView extends GetView<HomeController> {
         title: const Text('BLE Scanner'),
         centerTitle: true,
         actions: [
-          Obx(() => IconButton(
-                onPressed: controller.recordState.value == null ? controller.discoveryDevice : null,
-                icon: const Icon(Icons.bluetooth_searching),
-              )),
+          GetBuilder<HomeController>(
+            id: 'bluetoothIcon',
+            builder: (_) => IconButton(
+              onPressed: controller.recordState == null ? _.discoveryDevice : null,
+              icon: const Icon(Icons.bluetooth_searching),
+            ),
+          ),
         ],
       ),
       drawer: const CustomAppDrawer(),
-      body: Obx(() => ListView.builder(
-            itemCount: controller.devices.length,
-            itemBuilder: (context, index) {
-              BWT901CL sensor = controller.devices[index] as BWT901CL;
+      body: GetBuilder<HomeController>(
+        id: 'deviceList',
+        builder: (_) => ListView.builder(
+          itemCount: _.devices.length,
+          itemBuilder: (context, index) {
+            if (_.devices[index] is BWT901CL) {
+              BWT901CL sensor = _.devices[index] as BWT901CL;
 
               return Obx(() => BWT901CLTile(
                     title: sensor.device.name ?? '',
                     unit: sensor.accelerationUnit,
                     onSetting: () => Get.defaultDialog(
                       title: sensor.device.name ?? '',
-                      content: IMUSettingDialog(
-                        unit: sensor.accelerationUnit,
-                        setUnit: (String unit) => controller.setUnit(sensor, unit),
-                        returnRate: sensor.frequency,
-                        setReturnRate: (int frequency) => controller.setReturnRate(sensor, frequency),
-                        calibrate: () => controller.calibrate(sensor),
-                        returnContents: sensor.returnContents,
-                        setReturnContents: (ReturnContents returnContents) => controller.setReturnContents(sensor, returnContents),
+                      content: BWT901ClSettingDialog(
+                        sensor: sensor,
                       ),
                     ),
-                    onClose: () => controller.disconnect(sensor),
-                    signal: controller.datas[index].map((e) => e as BWT901CLSignal).toList(),
+                    onClose: sensor.dispose,
+                    signal: _.datas[index].map((e) => e as BWT901CLSignal).toList(),
                   ));
-            },
-          )),
-      floatingActionButton: Obx(() => Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              if (controller.recordState.value == null)
-                FloatingActionButton(
-                  onPressed: () => controller.switchRecordState(null),
-                  child: const Icon(Icons.play_arrow_rounded),
-                ),
-              if (controller.recordState.value == true)
-                FloatingActionButton(
-                  onPressed: () => controller.switchRecordState(true),
-                  child: const Icon(Icons.pause_rounded),
-                ),
-              if (controller.recordState.value == false)
-                FloatingActionButton(
-                  onPressed: () => controller.switchRecordState(null),
-                  child: const Icon(Icons.play_arrow_rounded),
-                ),
-              if (controller.recordState.value == false) const SizedBox(width: 10),
-              if (controller.recordState.value == false)
-                FloatingActionButton(
-                  onPressed: () => controller.switchRecordState(false),
-                  child: const Icon(Icons.stop_rounded),
-                ),
-            ],
-          )),
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: GetBuilder<HomeController>(
+        id: 'fab',
+        builder: (_) => CustomFloatingActionButton(
+          state: _.recordState,
+          onPressed: _.switchRecordState,
+        ),
+      ),
     );
   }
 }
